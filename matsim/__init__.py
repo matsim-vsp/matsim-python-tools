956aa29928e1034957319fbe35a261192e8e6713
GabinRpt <gabin.raapoto@univ-eiffel.fr>
2022-07-22 16:16:01 +0200

    Added vehicle_reader function to __init__ file

--- matsim/__init__.py
+++ matsim/__init__.py
@@ -1,5 +1,6 @@
-from . import Events, Network, Plans, TripEventHandler, writers
+from . import Events, Network, Plans, Vehicle, TripEventHandler, writers
 
 read_network = Network.read_network
 event_reader = Events.event_reader
 plan_reader = Plans.plan_reader
+vehicle_reader = Vehicle.vehicle_reader
