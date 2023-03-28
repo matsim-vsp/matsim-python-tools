998906fd5c42f4d953bd5a6ad297f4c8da01f7b8
rakow <rakow@vsp.tu-berlin.de>
2023-03-28 13:30:28 +0200

    fixate random state

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -12,6 +12,8 @@ def test_asc_sampler():
     # variables are not needed for this test
     sampler = ASCSampler(None, None, None, None, None)
 
+    np.random.seed(0)
+
     utils = np.random.rand(1000, 4)
     ascs = np.zeros(4)
 
