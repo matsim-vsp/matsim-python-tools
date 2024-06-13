916a0571b70e95b4f00e2cd60c7bcdd6d0742ff2
rakow <rakow@vsp.tu-berlin.de>
2024-06-13 20:28:29 +0200

    implemented log-likelihood calculation

--- matsim/calibration/run_simulations.py
+++ matsim/calibration/run_simulations.py
@@ -9,13 +9,62 @@ from os import makedirs
 from time import sleep
 from typing import Union, Callable
 
+import pandas as pd
+import numpy as np
+
 METADATA = "run-simulations", "Utility to run multiple simulations at once."
 
 
-def process_results(directory):
+def process_results(runs):
     """Process results of multiple simulations"""
+    from sklearn.metrics import log_loss
+    from sklearn.preprocessing import LabelEncoder
 
-    print("Processing results in %s" % directory)
+    print("Processing results in %s" % runs)
+
+    dfs = None
+    for run in os.listdir(runs):
+        if not os.path.isdir(os.path.join(runs, run)):
+            continue
+        print("Processing run %s" % run)
+
+        df = pd.read_csv(os.path.join(runs, run, "analysis", "population", "mode_choices.csv"))
+        if dfs is None:
+            dfs = df
+        else:
+            dfs= dfs.merge(df, left_on=["person", "n", "true_mode"], right_on=["person", "n", "true_mode"], suffixes=("", "_%s" % run))
+
+    shares = dfs.groupby("true_mode").size() / len(dfs)
+    modes = shares.index
+
+    labels = LabelEncoder().fit(modes)
+    y_true = labels.transform(dfs["true_mode"])
+    y_null = np.tile(shares.to_numpy(), reps=(len(y_true), 1))
+    y_pred = np.zeros((len(y_true), len(modes)))
+    dists = dfs.euclidean_distance.to_numpy() / 1000
+
+    pred_cols = [c for c in dfs.columns if c.startswith("pred_mode")]
+    for p in dfs[pred_cols].itertuples():
+
+        for j, m in enumerate(modes):
+            c = 0
+            for col in pred_cols:
+               if getattr(p, col) == m:
+                     c += 1
+
+            y_pred[p.Index, j] = c / len(pred_cols)
+
+    result = [
+        ("Log likelihood", -log_loss(y_true, y_pred, sample_weight=dfs.weight, normalize=False), -log_loss(y_true, y_pred, sample_weight=dfs.weight * dists, normalize=False)),
+        ("Log likelihood (normalized)", -log_loss(y_true, y_pred, sample_weight=dfs.weight, normalize=True), -log_loss(y_true, y_pred, sample_weight=dfs.weight * dists, normalize=True)),
+        ("Log likelihood (null)", -log_loss(y_true, y_null, sample_weight=dfs.weight, normalize=False), -log_loss(y_true, y_null, sample_weight=dfs.weight * dists, normalize=False)),
+        ("Samples", len(dfs), sum(dists)),
+    ]
+
+    df = pd.DataFrame(result, columns=["Metric", "Value", "Distance weighted"]).set_index("Metric")
+    print(df)
+
+    df.to_csv(os.path.join(runs, "results.csv"), index=True)
 
 
 def run(jar: Union[str, os.PathLike],
@@ -103,7 +152,7 @@ def run(jar: Union[str, os.PathLike],
         finally:
             p.terminate()
 
-    process_results("eval_runs")
+    process_results("eval-runs")
 
 
 def setup(parser: argparse.ArgumentParser):
