a46b20b054f1b532a867e5333519c39674adf480
rakow <rakow@vsp.tu-berlin.de>
2024-09-10 09:13:40 +0200

    batch processing of the individual model

--- matsim/scenariogen/network/ref_model/individual.py
+++ matsim/scenariogen/network/ref_model/individual.py
@@ -1,18 +1,16 @@
 """ Reference model to fine tune individal links. This model does not use features and is not trasferable to other scenarios. """
 
-# Each link has its own target speed
-
+import jax.numpy as jnp
 
 params = None
 
-def score(params, inputs):
-    return params[inputs[0]]
-
-
 def batch_loss(params, inputs, targets):
-    error = 0
     inputs = inputs.astype(int)
-    for x, y in zip(inputs, targets):
-        preds = score(params, x)
-        error += (preds - y) ** 2
+
+    # Select parameters by index
+    preds = params[inputs].T
+
+    # Calculate the loss
+    error = jnp.sum((preds - targets) ** 2)
+
     return error
